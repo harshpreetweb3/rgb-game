9bf46e36607dc5ad5c92b47e6c748826a0d8eb39
Harshpreet Singh <harshpreetweb3@gmail.com>
2023-11-14 12:30:01 +0530

    Initial commit.

---
+++ src/rgb_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { rgb_backend } from "../../declarations/rgb_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await rgb_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
