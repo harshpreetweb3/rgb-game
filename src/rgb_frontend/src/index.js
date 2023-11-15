b076e2bf42ee27af7c5a4ab2187191708d786e23
Harshpreet Singh <harshpreetweb3@gmail.com>
2023-11-15 16:30:02 +0530

    rgb-project

--- src/rgb_frontend/src/index.js
+++ src/rgb_frontend/src/index.js
@@ -1,19 +1,140 @@
-import { rgb_backend } from "../../declarations/rgb_backend";
+import { rgb_backend } from '../../declarations/rgb_backend';
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
+var numSquares = 6;
+var colors = [];
+var pickedColor;
+var attempts = 0;
+var rightGuess = 0;
+var wrongGuess = 0;
 
-  button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await rgb_backend.greet(name);
 
-  button.removeAttribute("disabled");
+var squares = document.querySelectorAll(".square");
+var colorDisplay = document.querySelector("#color-display");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
+var easyButton = document.querySelector(".mode");
 
-  document.getElementById("greeting").innerText = greeting;
+init();
 
-  return false;
+function init() {
+	colorDisplay.textContent = pickedColor;
+	setupSquares();
+	setupMode();
+	reset();
+}
+
+resetButton.addEventListener("click", function () {
+	reset();
 });
+
+const score = document.getElementById("score");
+
+
+function setupSquares() {
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = colors[i];
+		squares[i].addEventListener("click", function () {
+			var clickedColor = this.style.backgroundColor;
+			if (clickedColor === pickedColor) {
+				messageDisplay.textContent = "Correct";
+				resetButton.textContent = "Play Again";
+				changeColors(pickedColor);
+				attempts = attempts + 1;
+				rightGuess++
+				rgb_backend.attemptsRightWrong(attempts, rightGuess, wrongGuess);
+				score.textContent = "Attempts: "+ attempts+ " Right Guess: "+ rightGuess + "   Wrong Guess: " + wrongGuess;
+				console.log(rightGuess, wrongGuess, attempts, 'r', 'w', 'a');
+			}
+			else {
+				this.style.backgroundColor = "#232323";
+				messageDisplay.textContent = "try again";
+				attempts++;
+				wrongGuess++;
+				rgb_backend.attemptsRightWrong(attempts, rightGuess, wrongGuess);
+				score.textContent = "Attempts: "+ attempts+ " Right Guess: "+ rightGuess + "   Wrong Guess: " + wrongGuess;
+				console.log(rightGuess, wrongGuess, attempts, 'r', 'w', 'a');
+			}
+		});
+	}
+}
+
+
+
+
+function setupMode() {
+	for (var i = 0; i < modeButtons.length; i++) {
+		modeButtons[i].addEventListener("click", function () {
+			for (var i = 0; i < modeButtons.length; i++) {
+				modeButtons[i].classList.remove("selected");
+			}
+			this.classList.add("selected");
+			if (this.textContent === "Easy") {
+				numSquares = 3;
+			}
+			else {
+				numSquares = 6;
+			}
+			reset();
+		});
+	}
+}
+
+function reset() {
+	colors = genRandomColors(numSquares);
+	pickedColor = chooseColor();
+	colorDisplay.textContent = pickedColor;
+	h1.style.backgroundColor = "#2C8E99";
+	resetButton.textContent = "New Colors";
+	messageDisplay.textContent = "";
+	for (var i = 0; i < squares.length; i++) {
+		if (colors[i]) {
+			squares[i].style.display = "block";
+			squares[i].style.backgroundColor = colors[i];
+		}
+		else {
+			squares[i].style.display = "none";
+		}
+	}
+}
+
+function changeColors(color) {
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = color;
+		h1.style.backgroundColor = color;
+	}
+}
+
+function chooseColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function genRandomColors(num) {
+	var arr = [];
+	for (var i = 0; i < num; i++) {
+		arr.push(makeColor());
+	}
+	return arr;
+}
+
+function makeColor() {
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+
+
+
+console.log("right", rightGuess);
+console.log("wrong", wrongGuess);
+console.log("attempts", attempts);
+
+
+console.log(rightGuess, wrongGuess, attempts, 'r', 'w', 'a');
+
